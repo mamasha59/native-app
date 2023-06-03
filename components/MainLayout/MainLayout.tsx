cd999f2d61bf6ee1615b2f977aa3e17e6147f7f7
Alesha Tikhonov <zukko961@gmail.com>
2023-06-03 13:55:29 +0300

    cleaned code

--- components/MainLayout/MainLayout.tsx
+++ components/MainLayout/MainLayout.tsx
@@ -1,13 +1,13 @@
 import { StatusBar } from "expo-status-bar";
-import { Text, ScrollView, Pressable, View } from "react-native";
+import { Text, ScrollView } from "react-native";
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback } from 'react';
 import { LinearGradient } from 'expo-linear-gradient';
 
 import TopBar from "../TopBar/TopBar";
-import { useRoute } from "@react-navigation/native";
+import Button from "../Button/Button";
 
 interface iMainLayout {
     children: React.ReactNode;
@@ -17,22 +17,8 @@ interface iMainLayout {
 }
 
 const MainLayout = ({ children, title, buttonBottomTitle, buttonAction }:iMainLayout) => {
-
-  const route = useRoute(); // берем именя маршрутов
-  const [show , setShow] = useState<boolean>(false);
-  
-  useEffect(()=>{ // показываем кнопку только на опр страницах
-    if (route.name === 'Recomendations' || route.name === 'FeedbackScreen') {
-      setShow(true);
-    } else {
-      setShow(false);
-    }
-  },[route.name])
-  
   // загружаем шрифт
-  const [fontsLoader] = useFonts({
-    'Geometria' : require('../../assets/fonts/geometria_extrabold.otf'),
-  });
+  const [fontsLoader] = useFonts({'Geometria' : require('../../assets/fonts/geometria_extrabold.otf')});
 
   const onLayoutRootView = useCallback(async () => { 
     if (fontsLoader) {
@@ -40,9 +26,7 @@ const MainLayout = ({ children, title, buttonBottomTitle, buttonAction }:iMainLa
       }
     }, [fontsLoader]);
 
-    if (!fontsLoader) {
-      return null;
-    }
+    if (!fontsLoader) return null;
 
   return (
     <LinearGradient
@@ -59,13 +43,8 @@ const MainLayout = ({ children, title, buttonBottomTitle, buttonAction }:iMainLa
             {children} 
         </ScrollView>
         <StatusBar style="auto"/>
-        <View className="absolute flex items-center left-0 right-0 bottom-5">
-          {show ? 
-            <Pressable onPress={() => buttonAction()} className="min-w-[300px] bg-[#4BAAC5] px-[53px] py-[18px] rounded-[89px]">
-              <Text className="text-[#FFFFFF] font-bold text-center text-base leading-5">{buttonBottomTitle}</Text>
-            </Pressable> : ''
-          }
-        </View>
+        <Button buttonBottomTitle={buttonBottomTitle} buttonAction={buttonAction}/>
+
       </SafeAreaView>
     </LinearGradient>
   );
